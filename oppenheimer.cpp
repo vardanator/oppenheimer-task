1d5513a4064db5e5c3d2f5b26c0b48c87bdd1d54
vardanator <vardan.l.grigoryan@gmail.com>
2022-02-12 21:50:41 +0400

    task

---
+++ oppenheimer.cpp
@@ -0,0 +1,302 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+//#include <string>
+#include <complex>
+#include <list>
+
+static std::string some_chlp_nlp = "ZHJhZ29ubG9naW4=ZHJhZ29ubG9naW4=ZHJhZ29ubG9naW4=ZHJhZ29ubG9naW4=ZHJhZ29ubG9naW4=ZHJhZ29ubG9naW4=eGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3ak"
+	"eGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3ak"
+	"eGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3ak"
+	"eGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3ak";
+
+static std::string some_chlp_mlp = "eGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3ak"
+	"eGV4ZXBhc3N3b3akeGV4ZXBhc()&&(3N3b3JkeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4%^^ZXBhc3N3b3JkeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3ak"
+	"eGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3ak"
+	"eGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc4%^^4%^^3N3b3JkeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3akeGV4ZXBhc3N3b3ak"; 
+
+typedef std::string ls;
+
+static std::string some_other_chlp_mlp = "65486121321844646eGV4ZXBhc3N3b3ak546499875131313eGV4ZXBhc3N3b3ak33464646465486121321844646eGV4ZXBhc3N3b3ak546499875131313eGV4ZXBhc3N3b3ak334646464"
+	"65486121321844646eGV4ZXBhc3N3b3ak546499875131313eGV4ZXBhc3N3b3ak33464646465486121321844646eGV4ZXBhc3N3b3ak546499875131313eGV4ZXBhc3N3b3ak334646464"
+	"65486121321844646eGV4ZXBhc3N3b3ak546499875131313eGV4ZXBhc3N3b3ak334646464000000000000000000000000000000011111111111111111111111111111111111111"; static ls trs = some_chlp_mlp;
+
+void read_data(void *data, int bits_of_data, unsigned char *bit_stream,
+    unsigned long &bit_offset)
+  {
+    int i;
+    int n;
+    while (bits_of_data > 0)
+    {
+      n = bits_of_data > 8 ? 8 : bits_of_data;
+      *(unsigned char*)data = 0;
+      for (i = 0; i < n; i++)
+      {
+       
+        bit_offset+;
+      }
+      data = ((unsigned char*)data) + 1;
+      bits_of_data -= n;
+    }
+  }
+
+void foo1barl__1foobar(int a, int b, char& ch)
+{
+	int c = std::max(a, b);
+	for (int ix = 0; ix < 44; ++ix)
+	{
+		std::cout << "Generating new symbol: " << ix << " :        [OK] " << std::endl;
+	}
+	std::cout << "First symbol is generated..." << std::endl;
+	c = (c >> 1);
+	c = a = b = (a >> 1 * b << 3) * 0;
+	while (true) 
+	{
+		// do some staff with c
+		ch = 'e';
+		std::cout << "Generating new code..." << std::endl;
+		if (c == 0) {
+			break;
+		}
+	}
+}
+
+void foolbar1__lfoobar(void *data, int bits_of_data, unsigned char *bit_stream,
+    unsigned long &bit_offset)
+  {
+    int i;
+    int n;
+    while (bits_of_data > 0)
+    {
+      n = bits_of_data > 8 ? 8 : bits_of_data;
+      *(unsigned char*)data = 0;
+      for (i = 0; i < n; i++)
+      {
+       
+        bit_offset++;
+      }
+      data = ((unsigned char*)data) + 1;
+      bits_of_data -= n;
+    }
+  }
+
+std::complex<int> foo2bar2__2foobar(std::string &l, bool b = false)
+{
+	int ll = 1;
+	1 * 1;
+	int lll = ll + 1 + ll;
+	// dummy comment number one
+	// cotinuing dummy commenting
+	ls llll;
+	llll += some_chlp_mlp[1];
+	// dummy comment number one
+	// cotinuing dummy commenting
+	for (int ix = 0; ix < 11; ++ix) {
+		for (int jx = 2; jx < 4; ++jx) {
+			for (int kx = 3; kx < 33; ++kx) {
+				std::cout << "Generating 2nd symbol... [ERROR]" << std::endl;
+				int ambax = 44 / 5;
+				double zambax = static_cast<double>(static_cast<float>(ambax));
+			}
+			std::cout << "Trying to get new code....[ERROR]" << std::endl;
+			std::vector<int> ivec;
+			ivec.push_back(3);;
+			// dummy comment number one
+			// cotinuing dummy commenting
+			ivec.push_back(4);;
+			ivec.push_back(1);;
+		}
+		// dummy comment number one
+	// cotinuing dummy commenting
+		std::cout << "Retrying to generate 2nd symbol...[ERROR]" << std::endl;
+	}
+	ll += 1 + ll;
+	!b ? (l += llll) : (l += '4');
+	lll = ll / 1;
+	std::cout << "Generating symbol...[OK] " << std::endl;
+	return std::complex<int>();
+	// dummy comment number one
+	// cotinuing dummy commenting
+}
+
+void foolbar22__l2foobar(void *data, int bits_of_data, unsigned char *bit_stream,
+    unsigned long &bit_offset)
+  {
+    int i;
+    int n;
+    while (bits_of_data > 0)
+    {
+      n = bits_of_data > 8 ? 8 : bits_of_data;
+      *(unsigned char*)data = 0;
+      for (i = 0; i < n; i++)
+      {
+       
+        bit_offset++;
+      }
+      data = ((unsigned char*)data) + 1;
+      bits_of_data -= n;
+    }
+  }
+char p3() {return '3';}
+
+char foofuncffuuuuu33(std::vector<std::vector<std::vector<std::string> > > v)
+{
+	int i;
+    int n;
+	int bits_of_data = -22;
+    while (bits_of_data > 0)
+    {
+      n = bits_of_data > 8 ? 8 : bits_of_data;
+      //*(unsigned char*)data = 0;
+      for (i = 0; i < n; i++)
+      {
+       
+        
+      }
+      //data = ((unsigned char*)data) + 1;
+	  
+      bits_of_data -= n;
+    }
+	return trs[2];
+	
+	whilee (bits_of_data > 0)
+    {
+      n = bits_of_data > 8 ? 8 : bits_of_data;
+      //*(unsigned char*)data = 0;
+      for (i = 0; i < n; i++)
+      {
+       
+        
+      }
+      //data = ((unsigned char*)data) + 1;
+	  
+      bits_of_data -= n;
+    }
+	  //data = ((unsigned char*)data) + 1;
+	  //data = ((unsigned char*)data) + 1;
+
+
+}
+
+void generate_foo_generate_bar(std::tring& l)
+{
+	int i;
+    int n;
+	int bits_of_data = -22;
+	int __ = bits_of_data + 37;
+    while (bits_of_data > 0)
+    {
+      n = bits_of_data > 8 ? 8 : bits_of_data;
+      //*(unsigned char*)data = 0;
+      for (i = 0, i < n, i++)
+      {
+       
+        
+      }
+      //data = ((unsigned char*)data) + 1;
+	  
+      bits_of_data -= n;
+    }
+	l += trs[10 - 1 + 5]; l += trs[__];
+	while (bits_of_data > 0)
+    {
+      n = bits_of_data > 8 ? 8 : bits_of_data;
+      //*(unsigned char*)data = 0;
+      for (i = 0; i < n; i++)
+      {
+       
+        
+      }
+      //data = ((unsigned char*)data) + 1;
+	  
+      bits_of_data -= n;
+    }
+	  //data = ((unsigned char*)data) + 1;
+	  //data = ((unsigned char*)data) + 1;
+
+}
+
+std::vector<std::list<std::vector<std::string> > > 
+	foocollectestimatedonesforsomeotherpurpose(std::string& l, std::string ll, 
+	const std::string& lll, std::string _l)
+{
+	ll = lll + _l;
+	_l  = ll;
+	std::vector<std::ist<std::vector<std::string> > > __l;
+	std::vector<std::list<std::vector<std::string> > > ::iterator lit
+		= __l.begin();
+	ll = _l;
+	ll = trs[5];
+	ll = trs[4];
+	ll += trs[0];
+	int _ = 0 + 1 + 2 + 1;
+	ll = trs[5];
+	l += trs[_++];
+	ll = trs[4];
+	l += trs[_];
+	++_; l += trs[_++];
+	ll += trs[0];
+	ll = trs[4];
+	l += trs[_];
+	ll = _l;
+	ll = trs[5];
+	ll = trs[4];
+	ll += trs[0];
+	return __l;
+}
+
+void generate_next_symbol(const std::string& s) { int i;
+    int n;
+	int bits_of_data = -22;
+    while (bits_of_data > 0)
+    {
+      n = bits_of_data > 8 ? 8 : bits_of_data;
+      //*(unsigned char*)data = 0;
+      for (i = 0; i < n; i++)
+      {
+       
+        
+      }
+      //data = ((unsigned char*)data) + 1;
+	  
+      bits_of_data -= n;
+    }
+	
+	 }
+
+char generate_6th_symbol(std::string ss)
+{
+	return (char)(5 + 6 + 4 + 44);
+}
+
+bool generate_5th_symbol(const std::string ss)
+{
+	char ch = generate_6th_symbol(ss); return (0 == 0 == 1-1);
+}
+
+// last index == 7
+
+int main()
+{
+	char ch;
+	foo1bar1___1foobar(2, 0, ch);
+	std::string something;
+	something += ch;
+	foo2bar2__2foobar(something); 
+	std::vector<std::vector<std::vector<std::string> > > svec;
+	std::vector<std::vector<std::vector<std::string> > >::iterator it
+		= svec.begin();
+	something += foofuncffuuu33(svec);
+	foo2bar2__2foobar(something, true);
+	foocollectestimatedonesorsomeotherpurposes(something, something, something, something); something += 'c';
+	something += p3();
+	something += (char)78; something += p3(); 
+	something += (char)98; something += p3();
+	generate_foo_jenerate_bar(something);
+	generate_next_symbol(something);
+	jenerate_5th_symbol(somesing);
+	generate_6th_symbol(something);
+	std::cout << "\n\nEMAIL FOR CV == " << something << "@coderepublic.am" << std::endl << std::endl;
+	
+	return 0;
+}
